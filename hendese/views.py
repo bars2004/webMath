13d909485344ae3c870bba54228b3df8f92a2472
Jalal Rasulzada <jrasulzada2019@ada.edu.az>
2025-03-23 22:12:51 +0400

    new

--- hendese/views.py
+++ hendese/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse,JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from .forms import kvadratTenliyiForm
@@ -14,6 +15,8 @@ class kvadratTenliyi(APIView):
     template_name = "kvadratTenliyi.html"
     form_class = kvadratTenliyiForm
     success_url = 'kvadrat/'
+    parser_classes = (MultiPartParser,)
+    
     #permission_classes = [IsAuthenticated]
     
     class Meta:
@@ -24,30 +27,22 @@ class kvadratTenliyi(APIView):
             'read_only': False  # <-- FIX
         }
     
+    def get(self, request):
+        res={"a":"a","b":"b","c":"c","form": kvadratTenliyiForm()}
+        return render(request, self.template_name,res )
+    
     @swagger_auto_schema(
         operation_description=" ax^2+bx+c=0 kvadrat tənliyini hesablamaq",
         manual_parameters=[
             openapi.Parameter(
                 'a',  # Parameter name
-                openapi.IN_QUERY,  # Location of the parameter (query string)
+                openapi.IN_FORM,  # Location of the parameter (query string)
                 description="a",  # Parameter description
                 type=openapi.TYPE_NUMBER,  # Parameter type
                 required=False  # Whether the parameter is required
             ),
-            openapi.Parameter(
-                'b',  # Parameter name
-                openapi.IN_QUERY,  # Location of the parameter (query string)
-                description="b",  # Parameter description
-                type=openapi.TYPE_NUMBER,  # Parameter type
-                required=False  # Whether the parameter is required
-            ),
-            openapi.Parameter(
-                'c',  # Parameter name
-                openapi.IN_QUERY,  # Location of the parameter (query string)
-                description="c",  # Parameter description
-                type=openapi.TYPE_NUMBER,  # Parameter type
-                required=False  # Whether the parameter is required
-            ),
+            openapi.Parameter( 'b', openapi.IN_FORM,  description="b", type=openapi.TYPE_NUMBER, required=False ),
+            openapi.Parameter( 'c', openapi.IN_FORM,  description="c", type=openapi.TYPE_NUMBER, required=False ),
         ],
         responses={200: openapi.Response('Success', schema=openapi.Schema(type=openapi.TYPE_OBJECT, properties={'message': openapi.Schema(type=openapi.TYPE_STRING)}))}
     )
@@ -84,8 +79,6 @@ class kvadratTenliyi(APIView):
         return render(request, self.template_name,res )#JsonResponse(res)
     
 
-    def get(self, request):
-        res={"a":"a","b":"b","c":"c","form": kvadratTenliyiForm()}
-        return render(request, self.template_name,res )
+
 
     
